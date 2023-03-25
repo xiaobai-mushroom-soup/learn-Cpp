1f72e9bd315cccf01e18389dc89e028b1764f1c4
grz <1605537551@qq.com>
2023-03-25 22:56:47 +0800

    captor01:done

---
+++ captor01/PC/PC.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main()
+{
+    /*
+    int sum = 0, val = 1;
+    while (val <= 10) {
+        sum += val;
+        ++val;
+        std::cout << sum, val;
+    }
+    std::cout << "1+到10和为" << sum ;
+
+    //for 
+    for (int i = 0; i <= 10; i++) {
+        std::cout << "i:" << i<<std::endl;
+    }
+    */
+/*
+    int sum = 0, val = 0;
+    while (std::cin >> val) 
+        sum += val;
+    
+        std::cout << "sum is:" << sum <<std:: endl;
+*/
+    /*
+    int sum = 0, val = 0;
+    while (std::cin >> val) {
+        sum += val;
+    }
+    std::cout << "sum is:" << sum << std::endl;
+    */
+  
+
+
+
+
+    return 0;
+}
---
+++ captor01/annotation/annotation.cpp
@@ -0,0 +1,13 @@
+﻿// annotation.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+// 单行注释
+/*
+* 界定符对注释
+*/
